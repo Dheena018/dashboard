5c54f48910d08daf2199b60772d1d15f359b4af8
dheena <deenatamilnathan007@gmail.com>
2023-05-17 15:31:13 +0530

    create next js project

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+// import { motion } from "framer-motion";
+// import { useContext, useState } from "react";
+import { useContext } from "react"
+import { useState } from "react"
+import {AiOutlineClose} from "react-icons/ai"
+import {AiFillGithub} from "react-icons/ai"
+import {BsGoogle} from "react-icons/bs"
+import {FaFacebookF} from "react-icons/fa"
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword  } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+import { AuthContext } from "../../context/AuthContext";
+import Swal from 'sweetalert2'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+// import { Formik, useFormik } from "formik";
+// import * as Yup from "yup";
+
+
+
+const Login = ()=>{
+
+  const [errors,setErrors] = useState(false);
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+
+  const navigate = useNavigate();
+
+  const {dispatch} = useContext(AuthContext);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+    signInWithEmailAndPassword (auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Saved!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info')
+      }
+    })
+
+    dispatch({type:"Login",payload:user})
+    navigate("/navbar")
+  })
+  .catch((error) => {
+    // Swal.fire('Any fool can use a computer')
+    setErrors(true)
+  });
+  };
+
+  // const Users =(
+  //       {
+  //         "email":"dheena@gmail.com",
+  //       "password":"Dheena@123"
+  //     },
+  //     {
+  //       "email":"akash@gmail.com",
+  //       "password":"Akash@123",
+  //     }
+  // )
+//   const formik = useFormik({
+//     initialValues:{
+//       email:"",
+//       password:""
+//     },
+// validationSchema:Yup.object({
+//   email:Yup.string().email("Invalid email Address").required("Email is required"),
+//   password:Yup.string().max(10, "Please correct password").required("password Didn't Match")
+// }),
+// onSubmit:(values)=>{
+//   console.log(values);
+
+//   if(Users.email === formik.values.email && Users.password===formik.values.password){
+//     console.log("log in success");
+//   }else{
+//     console.log('user invalid');
+//   }
+// }
+
+//   });
+
+//   console.log(formik);
+    return(
+        <div className="w-full h-screen shadow-md flex items-center justify-center bg-cyan-500">
+      <div
+        className="w-80 bg-white rounded-lg"
+      >
+        <div className="flex items-center justify-between p-2">
+          <span className="text-lg">Login with</span>
+          <p className="cursor-progress hover:shadow:md">
+            <AiOutlineClose />
+          </p>
+        </div>
+        <div className="p-2">
+          <ul className="flex justify-center items-center gap-[2rem] rounded-full ">
+            <li className="p-2 text-5xl bg-grey-800 text-black rounded-full">
+              <a href="Github">
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className=" p-3 text-3xl bg-red-600 text-white rounded-full">
+              <a href="Google">
+                <BsGoogle />
+              </a>
+            </li>
+            <li className="p-3 text-3xl bg-blue-800 text-white rounded-full">
+              <a href="Facebook">
+                <FaFacebookF />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex w-100 flex-row items-center p-2">
+          <hr className="w-3/6" />
+          <p className="text-xl p-4">or</p>
+          <hr className="w-3/6" />
+        </div>
+        <div className="w-full">
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <input type="email"
+              name="email"
+              placeholder="Email"
+              // value={formik.values.email}
+              // onChange={formik.handleChange}
+              onChange={(e)=>setEmail(e.target.value)}
+              className="p-3 px-9 text-xl border-2"
+              />
+            </div>
+           {/* <span className="text-red-500">Email is required</span> */}
+           <div className="form-control">
+              <input type="password"
+              name="password"
+              placeholder="password"
+              // value={formik.values.password}
+              // onChange={formik.handleChange}
+              onChange={(e)=>setPassword(e.target.value)}
+              className="p-3 px-9 text-xl border-2"
+              />
+            </div>
+            {/* {formik.errors.password && <span className="text-red-500">password is required</span>} */}
+           
+
+            <div className="p-2 flex items-center justify-center ">
+              <button
+                type="submit"
+                className="w-full p-3 text-white bg-sky-400 hover:bg-sky-500 rounded-sm"
+              >
+                LOGIN
+              </button>
+            </div>
+            {errors &&<span className="text-lg font-bold text-red-500">Wrong email or password !</span>}
+
+            <div className="flex border-2 border-inherit p-5 m-3 px-2 border-sm">
+              <input type="checkbox" className="w-12 border-neutral-800" />
+              <p className="flex px-2">I'am not a robot</p>
+            </div>
+            <div className="flex items-center justify-center flex-col p-2">
+              <p>
+                <span  className="opacity-40"> Looking to </span>
+                <Link to={"/signup"} className="text-blue-600">
+                  create an account ?
+                </Link>
+              </p>
+              <p>
+                <a href="1" className="text-blue-600">
+                  Forget Password ?
+                </a>
+              </p>
+            </div>
+          </form>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Login;
